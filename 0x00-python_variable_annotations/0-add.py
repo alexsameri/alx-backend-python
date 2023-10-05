2529a9406803f7385657c91be6df6482c8be442f
alexsameri <samerialexx@gmail.com>
2023-10-05 23:26:44 +0300

    Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 '''Modules of 0-add'''
+
+
 def add(a: float, b: float) -> float:
     '''Adds 2 floating numbers
     '''
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module for 1-concat'''
+
+
+def concat (str1: str, str2: str) -> str:
+    '''Concat two strings
+    '''
+    return str1 + str2
